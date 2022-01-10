5c84f65ac268ad33d4ee9e1a115ea17f0ecd3c56
nkleins <74414355+nkleins@users.noreply.github.com>
2022-01-10 13:43:41 +0100

    Updated Main Menu & Added Option to delay NPCFollow

--- Assets/Scripts/NPCFollow.cs
+++ Assets/Scripts/NPCFollow.cs
@@ -1,23 +1,35 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class NPCFollow : MonoBehaviour
 {
     //Transform that NPC has to follow
     public Transform transformToFollow;
+
+    public float delay = 15f;
     //NavMesh Agent variable
-    NavMeshAgent agent;
+    NavMeshAgent _agent;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(DelayAgent(delay));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         //Follow the player
-        agent.destination = transformToFollow.position;
+        if (_agent == null) return;
+        _agent.destination = transformToFollow.position;
+    }
+
+    IEnumerator DelayAgent(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _agent = GetComponent<NavMeshAgent>();
     }
 }
