710e5e7a1e8e1bbd401a3bd110d7c138897a7949
nkleins <74414355+nkleins@users.noreply.github.com>
2022-01-30 16:41:05 +0100

    Added wandering NPCs

--- Assets/Ressources/Scripts/CollisionCheck.cs
+++ Assets/Ressources/Scripts/CollisionCheck.cs
@@ -9,13 +9,16 @@ public class CollisionCheck : MonoBehaviour
     public AnimatorController postCollisionAnimation;
     void OnCollisionEnter(Collision collision)
     {
-        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        
         if (collision.gameObject.CompareTag("Player"))
         {
+            
             Animator animator = gameObject.GetComponent<Animator>();
             animator.runtimeAnimatorController = postCollisionAnimation;
             moveScript.SetActive(false);
+            GetComponent<NPCFollow>().enabled = false;
             menu.SetActive(true);
+            
         }
     }
 }
